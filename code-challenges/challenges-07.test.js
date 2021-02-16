bc2bf02276545aeefccb2642a929df34efab8271
adard2002 <adard2002@gmail.com>
2021-02-16 17:58:28 -0600

    got challenge 6 passing

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -79,7 +79,8 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
-
+  return arr.map((value) =>
+    Math.pow(2, value));
 };
 
 /* ------------------------------------------------------------------------------------------------
